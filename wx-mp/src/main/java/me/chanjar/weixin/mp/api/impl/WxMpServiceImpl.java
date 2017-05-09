2f9059824f2a8410fcb5a57d95b3f80d3e524340
Administrator <Administrator@PC-20170212QLAC>
2017-05-09 09:59:57 +0800

    修改获取微信用户信息的接口

--- wx-mp/src/main/java/me/chanjar/weixin/mp/api/impl/WxMpServiceImpl.java
+++ wx-mp/src/main/java/me/chanjar/weixin/mp/api/impl/WxMpServiceImpl.java
@@ -280,7 +280,7 @@ public class WxMpServiceImpl implements WxMpService {
   @Override
   public WxMpUser oauth2getUserInfo(WxMpOAuth2AccessToken oAuth2AccessToken, String lang) throws WxErrorException {
     StringBuilder url = new StringBuilder();
-    url.append("https://api.weixin.qq.com/sns/userinfo?");
+    url.append("https://api.weixin.qq.com/cgi-bin/user/info?");
     url.append("access_token=").append(oAuth2AccessToken.getAccessToken());
     url.append("&openid=").append(oAuth2AccessToken.getOpenId());
     if (lang == null) {
